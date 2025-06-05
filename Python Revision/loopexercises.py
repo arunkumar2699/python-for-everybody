778e07ae74997ad97215672f251860d4aaf9b805
Arun Kumar <arun.kumar2699@outlook.com>
2025-06-05 22:49:39 +0530

    File updated

--- Python Revision/loopexercises.py
+++ Python Revision/loopexercises.py
@@ -338,3 +338,23 @@ print("\n")
 
 
 # exercise 12:
+
+number = int(input("Enter a number to check if it is prime: "))
+number = int(number)
+
+is_prime = True
+
+if number < 2:
+    is_prime = False
+
+for i in range(2, int(number**0.5) + 1):
+    if number % i == 0:
+        is_prime = False
+        break
+if is_prime:
+    print(f"{number} is a prime number")
+else:
+    print(f"{number} is not a prime number")
+print("\n")
+
+# Result: This will check if the input number is prime by testing divisibility from 2 up to the square root of the number. It will print whether the number is prime or not
